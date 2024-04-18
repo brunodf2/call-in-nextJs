a2591351a070fc292ba131882367591d858fb1f4
x02359482173 <x02359482173@tcu.gov.br>
2024-04-18 00:50:42 -0300

    feat(usuario): tela acesso usuario

---
+++ src/pages/register/styles.ts
@@ -0,0 +1,12 @@
+import { Box, Text, styled } from '@ignite-ui/react'
+
+
+export const Container = styled('main', {
+    maxWidth: 572,
+    margin: '$20 auto $4',
+    padding: '0 $4'
+})
+
+export const Header = styled('div', {})
+
+export const Form = styled(Box, {})
